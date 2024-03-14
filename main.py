7bef89e36227cb56c234f8df2a74f9c43273cf9e
Yujin031009 <115140447+Yujin031009@users.noreply.github.com>
2024-03-14 22:18:40 +0900

    신유진 2022110309

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #main.py
 
 st_num = input("학번을 입력하세요: ")
+st_major = input("학과를 입력하세요: ")
 st_name = input("이름을 입력하세요: ")
 
-print("학번 : ", st_num, "학과 : | 이름 : ", st_name)
+print("학번 : ", st_num, "| 학과 : ",st_major, "| 이름 : ", st_name)
